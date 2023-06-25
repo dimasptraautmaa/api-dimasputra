ef7764a906865632d9fe6d13b5eb416ba503a310
dimasptrautmaa <dimasptraautmaa@gmail.com>
2023-06-26 02:07:17 +0700

    update

--- index.js
+++ index.js
@@ -2,22 +2,15 @@ import http from 'http'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import express from 'express'
-import { Server } from 'socket.io';
 import { router } from './routes/router.js';
 
 const app = express();
 const server = http.createServer(app)
-const socket = new Server(server)
 
 dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
 app.use(cors({credentials : true, origin : 'http://localhost:5173/'}))
 app.use(router)
-
-socket.on("connection", (socket) => {
-    socket.on("pesan", (result) => socket.broadcast.emit("pesan", result))
-    socket.on("username", (data) => socket.broadcast.emit("username", data))
-})
 
 server.listen(process.env.app_port, () => console.log('server up and running...'))
