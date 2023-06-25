9dcf1c48d8cd6654775a61182779aeec5b42eab5
dimasptrautmaa <dimasptraautmaa@gmail.com>
2023-06-25 23:14:17 +0700

    add feature realtime broadcast

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-import { router } from './routes/router.js';
-import express from 'express'
-import dotenv from 'dotenv'
 import http from 'http'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import { Server } from 'socket.io';
+import { router } from './routes/router.js';
 
 const app = express();
 const server = http.createServer(app)
+const socket = new Server(server, {cors : 'http://localhost:5173/'})
 
 dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({extended : true}))
-app.use(cors({credentials : true, origin : 'http://localhost:5173'}))
+app.use(cors({credentials : true, origin : 'http://localhost:5173/'}))
 app.use(router)
+
+socket.on("connection", (socket) => {
+    socket.on("pesan", (result) => socket.broadcast.emit("pesan", result))
+    socket.on("username", (data) => socket.broadcast.emit("username", data))
+})
 
 server.listen(process.env.app_port, () => console.log('server up and running...'))
